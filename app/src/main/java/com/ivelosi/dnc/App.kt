e50bd7347ecf8c958cf92040cfdfd4c963774768
jai-git4208 <jaiminpansal@gmail.com>
2025-10-03 17:43:51 -0700

    removed unused imports

--- app/src/main/java/com/ivelosi/dnc/App.kt
+++ app/src/main/java/com/ivelosi/dnc/App.kt
@@ -1,8 +1,7 @@
 package com.ivelosi.dnc
 
 import android.app.Application
-import com.ivelosi.dnc.data.AppContainer
-import com.ivelosi.dnc.data.AppDataContainer
+
 
 class App : Application() {
     lateinit var container: AppContainer
